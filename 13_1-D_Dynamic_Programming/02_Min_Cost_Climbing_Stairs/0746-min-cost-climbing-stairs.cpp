e08c2951585f85247086a3c004167c6726e6c269
Sarah Mathew <117502741+sarah20037@users.noreply.github.com>
2025-07-13 18:02:45 +0530

    Update 0746-min-cost-climbing-stairs.cpp

--- 13_1-D_Dynamic_Programming/02_Min_Cost_Climbing_Stairs/0746-min-cost-climbing-stairs.cpp
+++ 13_1-D_Dynamic_Programming/02_Min_Cost_Climbing_Stairs/0746-min-cost-climbing-stairs.cpp
@@ -34,20 +34,24 @@ Dynamic Programming:
 */
 
 class Solution {
-  public:
-    int minCostClimbingStairs(vector<int> &cost) {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n + 1);
         dp[0] = cost[0];
         dp[1] = cost[1];
 
         for (int i = 2; i <= n; ++i) {
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+            if (i == n)
+                dp[i] = min(dp[i - 1], dp[i - 2]);  // No cost at the top
+            else
+                dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
         }
 
-        return min(dp[n - 1], dp[n]);
+        return dp[n];
     }
 };
+
 
 // O(1) space
 // class Solution {
